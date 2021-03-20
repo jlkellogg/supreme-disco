f01b184bf89b27a758f4c7358eaa14d85e451d61
JLKellogg <JLKellogg@outlook.com>
2021-03-20 10:25:50 -0700

    math working for tip, total decimals not adding

---
+++ main.js
@@ -0,0 +1,28 @@
+function calculateTip() {
+    var billAmount = document.getElementById("billAmount").value;
+    var tipPercentage = document.querySelectorAll('input[name="tip"]');
+    // if (billAmount === "") {
+        //     alert("Please enter a bill amount.");
+        //     return;
+        // }
+    let selectedTip;
+        
+    for (const tip of tipPercentage){
+        if (tip.checked) {
+            selectedTip = tip.value;
+            break;
+        }
+    }
+        
+    var tip = (billAmount * selectedTip);
+    tip = tip.toFixed(2);
+    document.getElementById("tipAmount").innerHTML=tip;
+
+    var total = (parseInt(billAmount) + parseInt(tip));
+    total = total.toFixed(2);
+    document.getElementById("totalBillAmount").innerHTML=total;
+}
+
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+}
